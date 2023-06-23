3f069932c137ffccaff461e05098a8dc69e89ea1
Damilola Sanni <sannifortune11@gmail.com>
2023-06-23 12:47:42 +0100

    update

--- BookStore/Router/bookRouter.ts
+++ BookStore/Router/bookRouter.ts
@@ -1,9 +1,16 @@
 import express from "express";
-import { getAllBooks, getOneBook } from "../Controller/bookController";
+import {
+  deleteBook,
+  getAllBooks,
+  getOneBook,
+  updateBook,
+} from "../Controller/bookController";
 
 const router = express.Router();
 
 router.route("/getallbooks").get(getAllBooks);
 router.route("/getone/:bookId").get(getOneBook);
+router.route("/deletebook/:id").delete(deleteBook);
+router.route("/updatebook/:id").patch(updateBook);
 
 export default router;
